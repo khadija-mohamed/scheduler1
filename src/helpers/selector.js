10a827b769e4aa7da85a4733936a771bef2233c0
Khadija Mohamed <khadijam@live.ca>
2022-03-31 17:45:08 -0400

    updated and refractored helpers, final commit

--- src/helpers/selector.js
+++ src/helpers/selector.js
@@ -1,31 +1,31 @@
-export function getInterview(state,interview) {
-  if (interview) {
-    const stateUpdated = {...interview, interviewer:state.interviewers[interview.interviewer]}
-    return stateUpdated
-  } else {
-    return null
+function getAppointmentsForDay(state, day) {
+  const result = [];
+  const dayData = state.days.filter(d => d.name === day)
+  if (!dayData[0]) return result;
+  for (const a of dayData[0].appointments) {
+    result.push(state.appointments[a]);
   }
-}
- 
-export function getAppointmentsForDay(state, day) {
-  const filteredDays = state.days.filter( mappedDay => mappedDay.name === day);
-  if (!filteredDays.length) { return []; }
-  const appointmentArray = [];
-  for (let item in filteredDays[0].appointments){
- 
-    appointmentArray.push(state.appointments[filteredDays[0].appointments[item]])
-  }
-return appointmentArray
-}
-
-export  function getInterviewersForDay(state, day) {
-  if (!state.interviewers) return [];
-  const filteredDay = state.days.filter( mappedDay => mappedDay.name === day)[0];
-
-  if (!filteredDay) return [];
-
-  if (!filteredDay.interviewers) return [];
-
-  const result =  Object.values(state.interviewers).filter( interviewer => filteredDay.interviewers.includes(interviewer.id))
   return result;
-}
+};
+
+function getInterview(state, interview) {
+  if (interview) {
+    const interviewer = state.interviewers[interview.interviewer];
+    return { ...interview, interviewer };
+  }
+  return null;
+};
+
+function getInterviewersForDay(state, day) {
+  const result = [];
+  const dayData = state.days.filter(d => d.name === day)
+
+  if (!dayData[0]) return result;
+  for (const a of dayData[0].interviewers) {
+    result.push(state.interviewers[a]);
+  }
+  
+  return result;
+};
+
+export { getAppointmentsForDay, getInterview, getInterviewersForDay };
