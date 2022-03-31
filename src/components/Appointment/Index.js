2063a83e5ea2a9eeb6a3e88bdf62271e091c9c6b
Khadija Mohamed <khadijam@live.ca>
2022-03-31 12:03:21 -0400

    fixing logic for props.interviewer

--- src/components/Appointment/Index.js
+++ src/components/Appointment/Index.js
@@ -1,5 +1,16 @@
 import "./styles.scss"
-import React from "react"
-export default function Appointment () {
-  return (<article className="appointment"></article>)
-}
+import React, { Fragment } from 'react'
+import Header from "components/Appointment/Header"
+import Show from "components/Appointment/Show"
+import Empty from "components/Appointment/Empty"
+export default function Appointment (props) {
+  return (
+    <Fragment>
+  <Header time={props.time} />
+    {props.interview ? <Show 
+      student={props.interview.student}  
+      interviewer={props.interview.interviewer.name} 
+      interview={props.interview }/> : <Empty /> }
+  </Fragment>
+  )
+};
