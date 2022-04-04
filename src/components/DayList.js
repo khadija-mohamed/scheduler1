08998caf9304613e27b87bf79f571eb23971301b
Khadija Mohamed <khadijam@live.ca>
2022-04-04 04:22:39 -0400

    fixed indentation

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,14 +2,14 @@ import React from 'react'
 import DayListItem from 'components/DayListItem.js'
 
 export default function DayList(props) {
-const days = props.days.map(day => {
- return(<ul>
-<DayListItem 
-  name={day.name} 
-  spots={day.spots} 
-  selected={day.name === props.day}
-  setDay={props.setDay}/>
- </ul>)
-})
-return days
- }
+  const days = props.days.map(day => {
+    return(<ul>
+      <DayListItem 
+      name={day.name} 
+      spots={day.spots} 
+      selected={day.name === props.day}
+      setDay={props.setDay}/>
+      </ul>)
+      })
+      return days
+    }
